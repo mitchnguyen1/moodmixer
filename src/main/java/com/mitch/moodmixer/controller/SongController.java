292fffd6ad4516ca2a5e8998020883b916fc192f
mitch <mitch01nguyen@gmail.com>
2023-01-10 19:20:00 -0600

    adds test in controller

--- src/main/java/com/mitch/moodmixer/controller/SongController.java
+++ src/main/java/com/mitch/moodmixer/controller/SongController.java
@@ -48,5 +48,24 @@ public class SongController {
     public List<Song> findItemByGen(@PathVariable Integer id){
         return songService.findSongsByGenre(id);
     }
+
+    @RequestMapping("/first")
+    public String welcome() {
+        return "../../frontend/build/index";
+    }
+    @RequestMapping("/second")
+    public String second() {
+        return "../../frontend/build/index.html";
+    }
+
+    @RequestMapping("/third")
+    public String third() {
+        return "index";
+    }
+
+    @RequestMapping("/fourth")
+    public String fourth() {
+        return "index.html";
+    }
 }
 
