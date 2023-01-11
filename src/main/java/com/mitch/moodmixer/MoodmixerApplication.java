cb5f96f446f59e707777dac30894a39e1d4cae6b
mitch <mitch01nguyen@gmail.com>
2023-01-10 18:33:15 -0600

    adds controller class and mapping for html pages

--- src/main/java/com/mitch/moodmixer/MoodmixerApplication.java
+++ src/main/java/com/mitch/moodmixer/MoodmixerApplication.java
@@ -4,57 +4,31 @@ import com.mitch.moodmixer.repository.entity.Song;
 import com.mitch.moodmixer.service.SongService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class MoodmixerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoodmixerApplication.class, args);
 	}
-
-	private final SongService songService;
-
-	public MoodmixerApplication(SongService songService) {
-		this.songService = songService;
+	@RequestMapping("/")
+	public String index() {
+		return "../../frontend/build/index.html";
 	}
 
-	@GetMapping("/all")
-	public List<Song> getSongs(){
-		return songService.all();
+	@RequestMapping("/output")
+	public String output() {
+		return "output.html";
 	}
 
-	@PostMapping("/add")
-	public void addSong(@RequestBody Song song){
-		songService.add(song);
-	}
-
-	@PutMapping( "/{id}" )
-	public Song update(@RequestBody Song song, @PathVariable Integer id )
-	{
-		Optional<Song> temp = SongService.findById( id );
-		Song song1 = temp.get();
-		song1.setSong_name( song.getSong_name() );
-		song1.setArtist( song.getArtist() );
-		song1.setGenre( song.getGenre() );
-		song1.setLink(song.getLink());
-		return SongService.save(song1);
-	}
-	@GetMapping("/count-users")
-	public String getCountUsers(){
-		return "Number of users = ";
-	}
-	@DeleteMapping( "/{id}" )
-	public void delete(@PathVariable Integer id){
-		songService.delete(id);
-	}
-
-	@GetMapping("/findBy/{id}")
-	public List<Song> findItemByGen(@PathVariable Integer id){
-		return songService.findSongsByGenre(id);
+	@RequestMapping("/input")
+	public String input() {
+		return "frontend/build/input.html";
 	}
 }
